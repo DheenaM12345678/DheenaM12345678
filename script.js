b836543b9ac464510e67bec63ddbfd2b9e9444f8
DheenaM12345678 <dheenam12@gmail.com>
2024-12-13 14:51:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const btn = document.getElementById("btn");
+btn.addEventListener("click", () => {
+  document.querySelector("header ul").classList.toggle("show");
+});
+
+const toTop = document.querySelector(".back-top");
+
+window.addEventListener("scroll", () => {
+  if (pageYOffset > 75) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+});
+var icon=document.getElementById("dark");
+icon.onclick=function(){
+  document.body.classList.toggle("dark-theme");
+}
